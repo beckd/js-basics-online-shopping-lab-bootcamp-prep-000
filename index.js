de6528c4a601ab433ba7d77f9019be31e18781d6
Dave Beck <dbeck2@outlook.com>
2018-06-19 18:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(itemName) {
   var itemPrice = Math.floor((Math.random() * 100) + 1)
-    cart.push(new Object({[itemName:itemPrice]})
+    cart.push(new Object({[itemName]:itemPrice}))
   console.log(`${itemName} has been added to your cart.`);
   return cart
 };
