7b1eab52c8b830de718732c3f7f74c0c0d7572a7
Dave Beck <dbeck2@outlook.com>
2018-06-19 20:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
-function setCart(c) {
+function setCart() {
   cart = c;
   return cart;
 }
 
-function addToCart(itemName) {
-  var price = Math.floor(Math.random()*100)+1);
-  var newItem = {[itemName]: price};
-    cart.push(newItem);
-    console.log(`${itemName} has been added to your cart.`);
-  return cart
-};
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.random() * 100
+  var roundedPrice = Math.floor(price)
+  cart.push({[item]: roundedPrice})
+  console.log(item + " has been added to your cart.")
+  return cart;
+}
 addToCart('bananas')
 addToCart('pancake batter')
 addToCart('apples')
@@ -23,37 +27,3 @@ addToCart('daikon')
 addToCart('eggplant')
 addToCart('carrots')
 addToCart('ice cream')
-/*
-function viewCart() {
-  if (!cart.length) {
-    return "Your shopping cart is empty."
-  }
-  else {
-    var myCart = "In your cart, you have "
-      for (var i = 0, 1 = cart.length; i < 1; i++) {
-        for (var myList in cart[i]) {
-          myCart += '${myList} at $$ {cart[i][list]}'
-          if(i!==cart.length-1) {
-            myCart +=","
-          }
-          else {
-            myCart += "."
-          }
-        }
-      }
-  }
-  console.log(myCart)
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  var cardNumber = parseInt("123456789");
-}
-*/
